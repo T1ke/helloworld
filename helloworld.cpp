f8e4c4aa633b95270d335a2afd46764c63b01733
Patrik Tirkkonen <patrik.tirkkonen@tuni.fi>
2020-02-11 14:41:45 +0200

    for loop test

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
 
+void print5(int i);
 int main()
 {
     std::cout << "Hello World!" << std::endl;
+    print5(5);
+}
+
+void print5(int i)
+{
+    for (int j=0; j < i; j++)
+    {
+        std::cout << j << std::endl;
+    }
 }
